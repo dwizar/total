ad2241e80334de7ea7883a8ea541c32b3a4ff39f
wizard <petrakovna@gmail.com>
2022-11-24 18:12:03 +0300

    Debug 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int[] arrres = new int[];
+for (int i = 0; i < n; i++)
+ {
+    Console.Write("Введите строку" + i+1 + ": ");
+    arr[i] = Console.ReadLine();
+    if(arr[i].Length <= 3) arrres[] += arr[i];
+ }
+ Console.WriteLine("[" + string.Join(", ", arr) + "] -> " + "[" + string.Join(", ", arrres) + "]");
